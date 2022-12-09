26c7889fa37ed46ab29bedd59c434d20e3b08de7
email2ify <email2ify@yahoo.com>
2022-12-09 14:04:03 +0000

    debug the while true loop for 'try again' because the  y/n asnwer  was not responsive as expected

--- run.py
+++ run.py
@@ -1,56 +1,63 @@
-# Addition of two numbers
-def add(x, y):
-   
-   return x + y
-
-# Subtraction of two numbers
+# subtraction of two numbers
 def subtract(x, y):
-    
     return x - y
 
-# Multiplication of two numbers
+# addition of two numbers
+def add(x, y):
+    return x + y
+
+
+# multiplication of two numbers
 def multiply(x, y):
-    
     return x * y
 
-# Division of two numbers
+
+# division of two numbers
 def divide(x, y):
-    
     return x / y
 
-print("Welcome to Stanley simple calculator.")
+
+print("Welcome to Stanley's plain calculator.")
 print("Please select operation:")
-print("1.Addition Arithmetic")
-print("2.Subtraction Arithmetic")
+print("1.Subtraction Arithmetic")
+print("2.Addition Arithmetic")
 print("3.Multiplication Arithmetic")
 print("4.Division Arithmetic")
 
 while True:
-    # User imput
-    choice = input("Enter digit within operation:(1/2/3/4): ")
+    # user imput digit
+    select = input("Enter digit within operation:(1/2/3/4): ")
 
     # The four options for the user to addition, subtraction, multiplication and division
-    if choice in ('1', '2', '3', '4'):
+    if select in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
+        if select == '1':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
+
+        elif select == '2':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif select == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
+        elif select == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
 
-        # To confirm if the user wants to do another calculation
-        # The loops breaks with if answer is no
-        next_calculation = input("Calculate again? (y/n): ")
-        if next_calculation == "n":
-            break
 
-    else:
-        print("Invalid Input, try again")
+        # To confirm if the user wants to do another calculation
+        while True:
+            # main program
+            while True:
+                answer = str(input('Try again? (y/n): '))
+
+                if answer in ('y', 'n'):
+                   break
+                print("invalid input.")
+            if answer == 'y':
+                break
+            else:
+                print("Goodbye or would you try again?")
+                continue
